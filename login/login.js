0b17d2c32c0423e6b1c28cc2a589cf966bfe5fd6
Rezaheydari315 <rezaheydari315reza@gmail.com>
2024-12-01 18:34:40 +0330

    Initial commit

---
+++ login/login.js
@@ -0,0 +1,51 @@
+let home = document.querySelector("#click_home")
+let signup = document.querySelector("#click_sign")
+let name = document.querySelector("#login_name")
+let password = document.querySelector("#login_password")
+let loginbtn = document.querySelector("#button_login_footer")
+
+let valname = name.value
+let valpass = password.value
+let localinfo = localStorage.getItem(`'${valname}'`)
+
+home.addEventListener('click', function (event) {
+    event.preventDefault()
+    location.href = 'http://127.0.0.1:5500/home/home.html'
+})
+signup.addEventListener('click', function (event) {
+    event.preventDefault()
+    location.href = 'http://127.0.0.1:5500/sign%20up/sign-up.html'
+})
+
+function login() {
+    loginbtn.addEventListener('click', function (event) {
+        event.preventDefault()
+        let valname = name.value
+        let valpass = password.value
+
+        let localinfo = localStorage.getItem(`'${valname}'`)
+
+        if (JSON.parse(localinfo).valname === valname && JSON.parse(localinfo).valpass === valpass) {
+            swal({
+                title: "Success",
+                text: `${'Welcome'}`,
+                icon: "success",
+            }).then(function (res) {
+                if (res === true) {
+                    location.href = 'http://127.0.0.1:5500/home/home.html'
+                }
+            })
+        } 
+        else {
+            if (JSON.parse(localinfo).valname === valname && JSON.parse(localinfo).valpass !== valpass) {
+                swal({
+                    title: "incorrect",
+                    icon: "error",
+                })
+            }
+        }
+
+
+    })
+}
+login()
